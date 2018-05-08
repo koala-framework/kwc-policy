fef66798f5e2b75475144cfa4be1c3f480506993
David Neumaier <dn@vivid-planet.com>
2018-05-08 15:34:36 +0200

    move functions from global context into function context

--- NotificationBox/Kwc/Component.defer.js
+++ NotificationBox/Kwc/Component.defer.js
@@ -2,34 +2,35 @@
 var $ = require('jquery');
 var onReady = require('kwf/on-ready');
 
-function setCookieValue(key, value) {
-    var d = new Date();
-    d.setTime(d.getTime() + (30 * 24 * 60 * 60 * 1000));
-    var expires = 'expires=' + d.toUTCString();
-    document.cookie = key + '=' + value + ';' + expires + ';path=/';
-}
-
-function getCookieValue(key) {
-    var name = key + '=',
-        decoded = decodeURIComponent(document.cookie),
-        ca = decoded.split(';');
-    for (var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return '';
-}
-
 onReady.onRender('.kwcClass', function (el, config) {
+
+    var setCookieValue = function(key, value, daysUntilExpired) {
+        var d = new Date();
+        d.setTime(d.getTime() + (daysUntilExpired * 24 * 60 * 60 * 1000));
+        var expires = 'expires=' + d.toUTCString();
+        document.cookie = key + '=' + value + ';' + expires + ';path=/';
+    };
+
+    var getCookieValue = function(key) {
+        var name = key + '=',
+            decoded = decodeURIComponent(document.cookie),
+            ca = decoded.split(';');
+        for (var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return '';
+    };
+
     var
         notificationSeen = getCookieValue('notificationSeen'),
         notificationChanged = config.changeDate;
-    
+
     if (!notificationSeen || (new Date(notificationSeen) < new Date(notificationChanged))) {
         el.show();
         el.find('.kwcBem__accept').click(function(e) {
