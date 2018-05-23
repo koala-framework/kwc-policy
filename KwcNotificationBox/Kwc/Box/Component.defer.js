f5f42bf96046b5473a56c23e634b3d874d42b12d
Johannes Huber <jh@vivid-planet.com>
2018-05-23 08:31:51 +0200

    Update logic to initially set a cookie and show banner only after updates

--- KwcNotificationBox/Kwc/Box/Component.defer.js
+++ KwcNotificationBox/Kwc/Box/Component.defer.js
@@ -21,16 +21,24 @@ onReady.onRender('.kwcClass', function (el) {
         cookies.remove(key);
     };
 
+    if (!getCookieIsSet('notificationSeen')) {
+        var d = new Date();
+        setCookieValue('notificationSeen', d.toUTCString(), 30);
+    }
+
     var
         notificationSeen = getCookieValue('notificationSeen'),
         notificationChanged = el.data('date');
 
-    if (!notificationSeen || (new Date(notificationSeen) < new Date(notificationChanged))) {
+    if (new Date(notificationSeen) < new Date(notificationChanged)) {
+        console.log('show');
         el.show();
         el.find('.kwcBem__accept').click(function(e) {
             e.preventDefault();
             el.hide();
-            setCookieValue('notificationSeen', config.changeDate, 30);
+            var d = new Date();
+            removeCookie('notificationSeen');
+            setCookieValue('notificationSeen', d.toUTCString(), 30);
             var body = $('body');
             body.removeClass('kwfUp-showNotificationBox').addClass('kwfUp-notificationSeen');
             onReady.callOnContentReady((body), { action: 'widthChange' });
