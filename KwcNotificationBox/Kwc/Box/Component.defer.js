8b4acb78b78e7f99f61b0c1df6146056513f551e
Johannes Huber <jh@vivid-planet.com>
2018-05-30 14:27:47 +0200

    Update logic of how the notification box is shown and hidden
    Logic: The box will be shown to all visitors for 30 days, starting with the alteration date.
    Once a visitor clicks it away, the box will be shown again when the alteration date changes.

--- KwcNotificationBox/Kwc/Box/Component.defer.js
+++ KwcNotificationBox/Kwc/Box/Component.defer.js
@@ -5,23 +5,27 @@ var cookies = require('js-cookie');
 
 onReady.onRender('.kwcClass', function (el) {
 
-    if (!cookies.get('notificationSeen')) {
-        var d = new Date();
-        cookies.set('notificationSeen', d.toUTCString(), { expires: 30 });
+    // Logic: The box will be shown to all visitors for 30 days, starting with the alteration date.
+    // Once a visitor clicks it away, the box will be shown again when the alteration date changes.
+
+    var alterationDate = new Date(el.data('alteration-date'));
+    var expirationDate = new Date(alterationDate);
+        expirationDate.setDate(alterationDate.getDate() + 30);
+    var cookie = cookies.get('kwfUp-kwcNotificationBox');
+
+    if (cookie) {
+        var notificationSeen = new Date(alterationDate) < new Date(cookie);
     }
 
-    var
-        notificationSeen = cookies.get('notificationSeen'),
-        notificationChanged = el.find('.kwcBem__alterationDate').data('alteration-date');
-
-    if (new Date(notificationSeen) < new Date(notificationChanged)) {
+    if (!notificationSeen && alterationDate < new Date() && expirationDate > new Date()) {
         el.removeClass('kwcBem__hidden');
         el.find('.kwcBem__accept').click(function(e) {
             e.preventDefault();
             el.addClass('kwcBem__hidden');
+
             var d = new Date();
-            cookies.remove('notificationSeen');
-            cookies.set('notificationSeen', d.toUTCString(), { expires: 30 });
+            cookies.set('kwfUp-kwcNotificationBox', d.toUTCString());
+
             var body = $('body');
             body.removeClass('kwfUp-showNotificationBox').addClass('kwfUp-notificationSeen');
             onReady.callOnContentReady((body), { action: 'widthChange' });
